57aaef27acf39d65c669146ca72a911e73c54144
chalaschek <chalaschek@gmail.com>
2018-04-24 07:45:30 -0400

    Added initial mc integration

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -12,12 +12,16 @@ class Splash extends Component {
 
               <div className="email-form">
                 <h3>Stay informed</h3>
-                <form className="form-inline">
+
+                <form className="form-inline" action="//network.us18.list-manage.com/subscribe/post?u=8d02e49730f2e912f6396b790&amp;id=5734faf1f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate="">
                   <div className="form-group">
-                    <label for="formInlineSlackEmail" className="control-label" style={{display: "none"}}>Email Signup</label>
-                    <input type="email" id="formInlineSlackEmail" className="form-control"/>
+                    <label for="mce-EMAIL" className="control-label" style={{display: "none"}}>Email Signup</label>
+                    <input type="email" id="mce-EMAIL" name="EMAIL" className="form-control"/>
+                    <div style={{position: "absolute", left: "-5000px", "aria-hidden": "true"}}>
+                      <input type="text" name="b_8d02e49730f2e912f6396b790_5734faf1f1" tabindex="-1" value=""/>
+                    </div>
                   </div>
-                  <button type="button" className="btn btn-lg btn-primary">Subscribe</button>
+                  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="btn btn-lg btn-primary"/>
                 </form>
               </div>
             </div>
