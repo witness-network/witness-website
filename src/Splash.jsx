53db4843933fb7d4dbf5507f671cb91d3c3c9358
chalaschek <chalaschek@gmail.com>
2018-04-29 14:56:59 -0400

    Resolving test warnings

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Splash extends Component {
   render() {
     return (
-        <div className="splash" style={{"background-image": "url(./background.png)"}}>
+        <div className="splash" style={{"backgroundImage": "url(./background.png)"}}>
           <div className="content">
             <div>
               <h1 className="headline">Decentralized Asset Verification</h1>
@@ -13,12 +13,12 @@ class Splash extends Component {
               <div className="email-form">
                 <h3>Stay informed</h3>
 
-                <form className="form-inline" action="//network.us18.list-manage.com/subscribe/post?u=8d02e49730f2e912f6396b790&amp;id=5734faf1f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate="">
+                <form className="form-inline" action="//network.us18.list-manage.com/subscribe/post?u=8d02e49730f2e912f6396b790&amp;id=5734faf1f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" noValidate="">
                   <div className="form-group">
-                    <label for="mce-EMAIL" className="control-label" style={{display: "none"}}>Email Signup</label>
+                    <label htmlFor="mce-EMAIL" className="control-label" style={{display: "none"}}>Email Signup</label>
                     <input type="email" id="mce-EMAIL" name="EMAIL" className="form-control"/>
-                    <div style={{position: "absolute", left: "-5000px", "aria-hidden": "true"}}>
-                      <input type="text" name="b_8d02e49730f2e912f6396b790_5734faf1f1" tabindex="-1" value=""/>
+                    <div style={{position: "absolute", left: "-5000px", "ariaHidden": "true"}}>
+                      <input type="text" name="b_8d02e49730f2e912f6396b790_5734faf1f1" tabIndex="-1" value=""/>
                     </div>
                   </div>
                   <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="btn btn-lg btn-primary"/>
